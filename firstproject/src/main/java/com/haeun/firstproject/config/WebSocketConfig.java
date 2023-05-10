3a060b7206058a34f9b2d891d3f18dd9487ddfbf
jhe2426 <jhe2426@naver.com>
2023-05-10 09:55:26 +0900

    23.05.10 09:50

--- firstproject/src/main/java/com/haeun/firstproject/config/WebSocketConfig.java
+++ firstproject/src/main/java/com/haeun/firstproject/config/WebSocketConfig.java
@@ -5,13 +5,25 @@ import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
+import com.haeun.firstproject.provider.WebSocketProvider;
+
+import lombok.RequiredArgsConstructor;
+
 @EnableWebSocket
 @Configuration
+@RequiredArgsConstructor
 public class WebSocketConfig  implements WebSocketConfigurer{
+
+    private final WebSocketProvider webSocketProvider;
+
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
         
+        //첫 번째 인자에는 핸들러 객체가 들어와야함 두 번째 인자에는 URL지정해서 넣으면 됨(endPoint 작성하면 됨)
+        registry
+            .addHandler(webSocketProvider, "/web-socket")
+            .setAllowedOrigins("*"); //모든 출처에 대해서 허용한 것
     }
     
 }
--- firstproject/src/main/java/com/haeun/firstproject/provider/WebSocketProvider.java
+++ firstproject/src/main/java/com/haeun/firstproject/provider/WebSocketProvider.java
@@ -1,5 +1,8 @@
 package com.haeun.firstproject.provider;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
@@ -10,23 +13,41 @@ import org.springframework.web.socket.handler.TextWebSocketHandler;
 @Component
 public class WebSocketProvider extends TextWebSocketHandler{ // WebSocket은 Handler라서 Handler폴더를 하나 생성후 거기 안에 정의해두는 것이 좋음
     
-    //* 핸들러 만들어줘야하는 3가지가 있음
+    //handleBinaryMessage 파일 전송해주는 핸들러도 있음
+    //* ws: 웹소켓 서버를 사용하기 때문에 주소에 http가 아니라 ws;//localhost:4040/web-socket으로 요청을 보내야 함
+    //* 핸들러 만들어 줘야하는 3가지가 있음
     
+
+    private List<WebSocketSession> sessionList = new ArrayList<>();
+
     //* 1. 연결
     @Override
 	public void afterConnectionEstablished(WebSocketSession session) throws Exception {
-	
+         //Request에 존재하는 Headers와 동일한 것 키를 통해서 값을 가져올 수  있음
+         //getFirst를 이용해서 키에 대한 값을 가져올 수  있음
+        String room = session.getHandshakeHeaders().getFirst("room");
+        sessionList.add(session);
+        
+        System.out.println(room + " / " + session.getId() + " Web Socket Connected!!");
     }
     
     //* 2. 메세지 송수신    
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
-	
+        //System.out.println(message.getPayload());
+        //실제로 메세지 송수신 방법
+        for (WebSocketSession sessionItem: sessionList) 
+            sessionItem.sendMessage(message);
+        
     }
     
     //* 3. 연결 해제
     @Override
 	public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
-	
+        System.out.println(session.getId() + " Web Socket Closed!!");
+        for (WebSocketSession sessionItem: sessionList) {
+            if (sessionItem.equals(session))
+                sessionList.remove(sessionItem);
+        }
     }
 }
